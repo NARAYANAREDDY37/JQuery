e425439931170ad34d2c11281702a2d52e597ee3
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-06-27 15:26:16 +0530

    first commit

---
+++ selectors/script.js
@@ -0,0 +1,18 @@
+// $('h1').hide();
+// $('h1#heading1').hide();
+// $('.heading2').hide();
+// $('p span').css('color', 'red');
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:last').css('color', 'blue');
+$('ul#list li:even').css('color', 'green');
+$('ul#list li:odd').css('background-color', 'orange');
+$('ul#list li:nth-child(2n)').css('list-style', 'none');
+
+$(':button').css('background', 'pink');
+
+$('[href]').css('color', 'red');
+$('a[href="http://yahoo.com"]').css('color', 'green');
+
+
+//Hides everything
+// $('*').hide();
